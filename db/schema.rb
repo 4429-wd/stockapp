fa3cd1b3691d2a523df6f23aa0e017f6e443ca1e
4429-wd <w4429d0gken@gmail.com>
2020-12-01 22:16:15 +0900

    edit-user

--- db/schema.rb
+++ db/schema.rb
@@ -57,8 +57,4 @@ ActiveRecord::Schema.define(version: 2020_11_14_090614) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "group_users", "groups"
-  add_foreign_key "group_users", "users"
-  add_foreign_key "messages", "groups"
-  add_foreign_key "messages", "users"
 end
