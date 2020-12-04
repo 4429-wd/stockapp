59392df0d1aaafe8047483eea3aa7c9b94a43f2c
4429-wd <w4429d0gken@gmail.com>
2020-12-04 19:40:17 +0900

    fix-controller

--- db/migrate/20201114090614_create_group_users.rb
+++ db/migrate/20201114090614_create_group_users.rb
@@ -1,8 +1,8 @@
 class CreateGroupUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :group_users do |t|
-      t.integer :group_id, foreign_key: true
-      t.integer :user_id, foreign_key: true
+      t.references :group, foreign_key: true
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
