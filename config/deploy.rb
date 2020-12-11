172967ed3600d44ae9f33e22d6ef688f44e4824b
4429-wd <w4429d0gken@gmail.com>
2020-12-11 09:45:29 +0900

    delete-capistrano3

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :bundle_gemfile,  "server/Gemfile"
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/id_rsa.pem）>'] 
+                  keys: ['~/.ssh/stockapp.pem）>'] 
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
