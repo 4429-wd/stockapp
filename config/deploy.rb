71f191b845527b52e7ed11b1ff12377bc4dc0a5f
4429-wd <w4429d0gken@gmail.com>
2020-12-26 19:11:05 +0900

    deploy_to

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,13 +2,14 @@
 lock "~> 3.14.1"
 
 set :application, "stockapp"
+set :deploy_to, 'var/www/rails/stockapp/'
 set :repo_url,  'git@github.com:4429-wd/stockapp.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
-set :bundle_gemfile,  "server/Gemfile"
+# set :bundle_gemfile,  "server/Gemfile"
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
