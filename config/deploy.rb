313b85177b2bfe72b6ef5666df71a5a319632c2c
4429-wd <w4429d0gken@gmail.com>
2021-01-16 16:01:06 +0900

    id_rsa

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,8 +13,8 @@ set :rbenv_ruby, '2.5.1'
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/stockapp.pem']
-                  
+                  keys: ['~/.ssh/id_rsa']
+
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
